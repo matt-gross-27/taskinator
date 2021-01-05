e005493ecd595bb0073c99cfb5a8f8a7331db955
Matt Gross <mbgross111@gmail.com>
2021-01-04 18:46:40 -0800

    data validate form inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,29 @@ var taskFormHandler = function() {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  
+  // check if input values are empty strings
+  if(!taskNameInput || !taskTypeInput) {
+    alert("Tasks need names and types");
+    return false;
+  };
   //package up data as an object
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
-  };
-  
+  };  
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
+  //clear the form
+  formEl.reset();
 };
 
 var createTaskEl = function(taskDataObj){
   // create list item
   var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
+  listItemEl.className = "task-item";  
   //create a div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
-  //add HTML content
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
   //add entire li to list
