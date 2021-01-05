e601b62399e8ffe8aeb5d4fd9f39b746927c2dd4
Matt Gross <mbgross111@gmail.com>
2021-01-04 16:25:20 -0800

    fadd tasks through form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+  event.preventDefault();
   var taskItemEl = document.createElement("li");
   taskItemEl.className = "task-item";
   taskItemEl.textContent = "What the Task!?";
   tasksToDoEl.appendChild(taskItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler)
