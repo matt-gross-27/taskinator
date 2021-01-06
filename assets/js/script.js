1a619e1dbf70a7645261bcfb969d5320d1905766
Matt Gross <mbgross111@gmail.com>
2021-01-05 16:48:47 -0800

    add data task id to task li elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -24,14 +25,19 @@ var taskFormHandler = function() {
 var createTaskEl = function(taskDataObj){
   // create list item
   var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";  
+  listItemEl.className = "task-item";
+  //add task id as a custom attribute
+  listItemEl.setAttribute("data-task-id",taskIdCounter);
   //create a div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class = 'task-type'>" + taskDataObj.type + "</span>";
+  //add div to li
   listItemEl.appendChild(taskInfoEl);
   //add entire li to list
   tasksToDoEl.appendChild(listItemEl);
+  // increase task counter for next unique id
+  taskIdCounter ++
 };
 
 formEl.addEventListener("submit", taskFormHandler);
